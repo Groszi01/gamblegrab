61b61f18c8121cc5545fa350de806ee71d965014
Groszi01 <oruak@protonmail.com>
2025-01-29 11:04:42 +0100

    SCRUM-45 SQL csatlakoztatás Express.js-ben

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  
+  host: 'localhost',
+  user: 'root',
+  password: null,
+  database: 'gamblegrab'
+}) 
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  //connection.query('SELECT * FROM users', function (error, results, fields) {
+    //if (error) throw error;
+   // console.log('The solution is: ', results);
+  //});
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,5 +31,8 @@ const port = process.env.PORT || 8080;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+
+
 
 app.listen(port);
