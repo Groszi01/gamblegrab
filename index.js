6961cd36cf82ca1cc23fc5fba108f9ad828eeaf3
Groszi01 <oruak@protonmail.com>
2025-05-21 12:24:14 +0200

    json db

--- index.js
+++ index.js
@@ -1,83 +1,21 @@
 
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: null,
-  database: 'gamblegrab'
-}) 
-
-
-function logInUser(password, email,) {
-  let loginSuccess;
-  connection.query('SELECT id, email FROM users WHERE jelszo = ? AND email = ?', [password, email], function (error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    if (results.length > 0) {
-      loginSuccess = true;
-    } else {
-      loginSuccess = false;
-    }
-    console.log(results)
-  });
-}
-
-function createUser(password, email) {
-  connection.query('INSERT INTO users (jelszo, email) VALUES (?, ?)', [password, email], function (error, results, fields) {
-    if (error) throw error;
-});
-}
-
-function checkBalance(email) {
-  connection.query('SELECT egyen FROM users WHERE email = ?', [email], function (error, results, fields) {
-    if (error) throw error;
-    console.log(results)
-  
-  });
-}
-
-function removeFromBalance(email, amount) {
-  connection.query('UPDATE users SET egyen = egyen - ? WHERE email = ?', [amount, email], function (error, results, fields) {
-    if (error) throw error;
-  });
-}
- 
-function addToBalance(email, amount) {
-  connection.query('UPDATE users SET egyen = egyen + ? WHERE email = ?', [amount, email], function (error, results, fields) {
-    if (error) throw error;
-  });
-}
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  //connection.query('SELECT * FROM users', function (error, results, fields) {
-    //if (error) throw error;
-   // console.log(results);
-  //});
-  console.log('connected as id ' + connection.threadId);
-
-
- // logInUser('jelszo123', 'kiskuki12@gmail.com');
- // removeFromBalance('kiskuki12@gmail.com', 1000);
-
-
-
-});
-
-
+const users = require('./users.json');
+const fs = require('fs');
+console.log(JSON.stringify(users));
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "style.css"));
+});
 
 
 
